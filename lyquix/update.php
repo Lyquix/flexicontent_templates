a86fcfcec6df9741ef8afc9d30f0659b119c822b
Ruben Reyes <ruben.reyes@lyquix.com>
2015-07-14 14:41:43 -0400

    Clean up update.php

--- lyquix/update.php
+++ lyquix/update.php
@@ -1,26 +1,33 @@
 <?php
 
-$url = 'https://github.com/Lyquix/flexicontent_templates/archive/master.zip';
+// download the latest from github
+file_put_contents("master.zip", fopen('https://github.com/Lyquix/flexicontent_templates/archive/master.zip', 'r'));
 
-file_put_contents("master.zip", fopen($url, 'r'));
-
+// new zip file object
 $zip = new ZipArchive;
-$res = $zip -> open('master.zip');
 
-if ($res === TRUE) {
+if ($zip -> open('master.zip') === TRUE) {
+	// no errors opening the file, extract to tmp directory
 	$zip -> extractTo('tmp/');
 	$zip -> close();
+	
+	// copy fresh files to base templates
 	copy_dir('tmp/flexicontent_templates-master/lyquix', './');
 	copy_dir('tmp/flexicontent_templates-master/json', '../json');
 	copy_dir('tmp/flexicontent_templates-master/custom', '../custom');
+	
+	// find any custom-* template directory and copy custom directory there
 	$custom_dirs = glob('../custom-*', GLOB_ONLYDIR);
 	foreach($custom_dirs as $dst) {
 		copy_dir('tmp/flexicontent_templates-master/custom', $dst);
 	}
+	
+	// delete zip file and tmp directory
+	unlink('master.zip');
+	delete_dir('tmp');
 }
 
-unlink('master.zip');
-delete_dir('tmp');
+
 
 function copy_dir($src, $dst) {
 	$dir = opendir($src);
