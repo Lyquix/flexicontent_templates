cf4f640374d94ceb9501034b44444828eff569a0
Ruben Reyes <ruben.reyes@lyquix.com>
2015-07-15 15:44:53 -0400

    Added gitignore to skip custom files. Modified category and item files to load custom functions.

--- custom/item.php
+++ custom/item.php
@@ -2,28 +2,9 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-class lyquixFlexicontentTmplCustom {
-	
-	function customFieldRendering(&$item, &$field) {
-		
-		$html = '';
-		
-		switch ($field->name) {
-			
-			/*
-			case 'field_name':
-				// your custom code for field_name here
-				break;
-			*/
-			
-			default:
-				break;
-				
-		}
-		return $html;
-		
-	}
-	
+// if item-custom.php file exists, include it
+if(file_exists(__DIR__ . '/item-custom.php')) {
+	include __DIR__ . '/item-custom.php';
 }
 
 include(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'templates'.DS.'lyquix'.DS.'item.php');
