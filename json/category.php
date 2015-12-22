1ba9a606745c0392a31995cb5991fd85dc9b7a02
Ruben Reyes <ruben.reyes@lyquix.com>
2015-12-22 16:15:41 -0500

    Updates to JSON template
    - Fields to control what parameters of items and categories are included in output
    - Item fields include label, value and display properties

--- json/category.php
+++ json/category.php
@@ -6,65 +6,66 @@ $doc = &JFactory::getDocument();
 $doc -> setMimeEncoding('application/json');
 
 // get category params
-$catparams = json_decode($this -> category -> params);
+$category = $this -> category;
+$url = JRoute::_(JURI::base() . FlexicontentHelperRoute::getCategoryRoute($category -> slug));
+$catparams = json_decode($category -> params);
+$json = array();
+$json['layout'] = 'category';
+if($this -> params -> get ('display_cat_id', 1)) $json['id'] = $category -> id;
+if($this -> params -> get ('display_cat_title', 1)) $json['title'] = $category -> title;
+if($this -> params -> get ('display_cat_alias', 1)) $json['alias'] = $category -> alias;
+if($this -> params -> get ('display_cat_description', 1)) $json['description'] = $category -> description;
+if($this -> params -> get ('display_cat_image', 1)) $json['image'] = $catparams -> image ? JURI::base() . $catparams -> image : '';
+if($this -> params -> get ('display_cat_created', 1)) $json['created'] = $category -> created_time;
+if($this -> params -> get ('display_cat_modified', 1)) $json['modified'] = $category -> modified_time;
+if($this -> params -> get ('display_cat_metadesc', 1)) $json['metadesc'] = $category -> metadesc;
+if($this -> params -> get ('display_cat_metakey', 1)) $json['metakey'] = $category -> metakey;
+if($this -> params -> get ('display_cat_url', 1)) $json['url'] = $url;
+if($this -> params -> get ('display_cat_json', 1)) $json['json'] = $url . (strpos($url, '?') ? '&' : '?') . 'clayout=' . JRequest::getVar('clayout') . '&tmpl=' . JRequest::getVar('tmpl');
+if($this -> params -> get ('display_cat_params', 0)) $json['params'] = $catparams;
 
-$json = array(
-	'layout' => 'category',
-	'id' => $this -> category -> id,
-	'title' => $this -> category -> title,
-	'alias' => $this -> category -> alias,
-	'description' => $this -> category -> description,
-	'created' => $this -> category -> created_time,
-	'modified' => $this -> category -> modified_time,
-	'metadesc' => $this -> category -> metadesc,
-	'metakey' => $this -> category -> metakey,
-	'url' => JRoute::_(JURI::base() . FlexicontentHelperRoute::getCategoryRoute($this -> category -> slug)),
-	'json' => JRoute::_(JURI::base() . FlexicontentHelperRoute::getCategoryRoute($this -> category -> slug) . '&clayout=' . JRequest::getVar('clayout') . '&tmpl=' . JRequest::getVar('tmpl')),
-	'image' => JURI::base() . $catparams -> image,
-	'items' => array()
-);
-
+// process category items
+$json['items'] = array();
 foreach($this -> items as $item) {
 	
-	$fields = array();
-	
-	if(isset($item -> positions)) {
-		foreach($item -> positions as $position) {
-			
-			foreach($position as $field) {
-				
-				if($item -> fields[$field -> name] -> iscore) {
-					$fields[$field -> name] = array(
-						'label' => $field -> label,
-						'value' => $item -> {$field -> name}
-					);
+	$url = JRoute::_(JURI::base() . FlexicontentHelperRoute::getItemRoute($item -> slug, $item -> categoryslug));
+	$json_item = array();
+	$json_item['layout'] = 'item';
+	if($this -> params -> get ('display_item_id', 1)) $json_item['id'] = $item -> id;
+	if($this -> params -> get ('display_item_title', 1)) $json_item['title'] = $item -> title;
+	if($this -> params -> get ('display_item_alias', 1)) $json_item['alias'] = $item -> alias;
+	if($this -> params -> get ('display_item_author', 1)) $json_item['author'] = $item -> author;
+	if($this -> params -> get ('display_item_description', 1)) $json_item['description'] = $item -> text;
+	if($this -> params -> get ('display_item_created', 1)) $json_item['created'] = $item -> created;
+	if($this -> params -> get ('display_item_modified', 1)) $json_item['modified'] = $item -> modified;
+	if($this -> params -> get ('display_item_metadesc', 1)) $json_item['metadesc'] = $item -> metadesc;
+	if($this -> params -> get ('display_item_metakey', 1))  $json_item['metakey'] = $item -> metakey;
+	if($this -> params -> get ('display_item_url', 1)) $json_item['url'] = $url;
+	if($this -> params -> get ('display_item_json', 1)) $json_item['json'] = $url . (strpos($url, '?') ? '&' : '?') . 'ilayout=' . JRequest::getVar('clayout') . '&tmpl=' . JRequest::getVar('tmpl');
+	if($this -> params -> get ('display_item_params', 0)) $json_item['params'] = json_decode($item -> params);
+	if($this -> params -> get ('display_item_fields', 1)) {
+		$fields = array();
+		if(isset($item -> positions)) {
+			foreach($item -> positions as $position) {
+				foreach($position as $field) {
+					$fields[$field -> name] = array();
+					if($this -> params -> get ('display_item_field_label', 1)) $fields[$field -> name]['label'] = $field -> label;
+					if($this -> params -> get ('display_item_field_value', 1)) {
+						if($item -> fields[$field -> name] -> iscore) {
+							$fields[$field -> name]['value'] = $item -> {$field -> name};
+						}
+						else {
+							$fields[$field -> name]['value'] = $item -> fieldvalues [$field -> id];
+						}
+					}
+					if($this -> params -> get ('display_item_field_display', 1)) $fields[$field -> name]['display'] = $field -> display;
+					
 				}
-				else {
-					$fields[$field -> name] = array(
-						'label' => $field -> label,
-						'value' => $item -> fieldvalues [$field -> id]
-					);
-				}
-				
 			}
-			
 		}
+		$json_item['fields'] = $fields;
 	}
-	
-	$json['items'][] = array(
-		'layout' => 'item',
-		'id' => $item -> id,
-		'title' => $item -> title,
-		'alias' => $item -> alias,
-		'author' => $item -> author,
-		'created' => $item -> created,
-		'modified' => $item -> modified,
-		'metadesc' => $this -> metadesc,
-		'metakey' => $this -> metakey,
-		'url' => JRoute::_(JURI::base() . FlexicontentHelperRoute::getItemRoute($item -> slug, $item -> categoryslug)),
-		'json' => JRoute::_(JURI::base() . FlexicontentHelperRoute::getItemRoute($item -> slug, $item -> categoryslug) . '&ilayout=' . JRequest::getVar('clayout') . '&tmpl=' . JRequest::getVar('tmpl')),
-		'fields' => $fields
-	);
+	$json['items'][] = $json_item;
 }
 
 if (JRequest::getVar('callback', '') != '') {
--- json/item.php
+++ json/item.php
@@ -5,45 +5,45 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 $doc =& JFactory::getDocument();
 $doc->setMimeEncoding('application/json');
 
-$fields = array();
+$item = $this -> item;
 
-if(isset($this -> item -> positions)) {
-	foreach($this -> item -> positions as $position) {
-		
-		foreach($position as $field) {
-			
-			if($this -> item -> fields[$field -> name] -> iscore) {
-				$fields[$field -> name] = array(
-					'label' => $field -> label,
-					'value' => $this -> item -> {$field -> name}
-				);
+$url = JRoute::_(JURI::base() . FlexicontentHelperRoute::getItemRoute($item -> slug, $item -> categoryslug));
+$json = array();
+$json['layout'] = 'item';
+if($this -> params -> get ('display_item_id', 1)) $json['id'] = $item -> id;
+if($this -> params -> get ('display_item_title', 1)) $json['title'] = $item -> title;
+if($this -> params -> get ('display_item_alias', 1)) $json['alias'] = $item -> alias;
+if($this -> params -> get ('display_item_author', 1)) $json['author'] = $item -> author;
+if($this -> params -> get ('display_item_description', 1)) $json['description'] = $item -> text;
+if($this -> params -> get ('display_item_created', 1)) $json['created'] = $item -> created;
+if($this -> params -> get ('display_item_modified', 1)) $json['modified'] = $item -> modified;
+if($this -> params -> get ('display_item_metadesc', 1)) $json['metadesc'] = $item -> metadesc;
+if($this -> params -> get ('display_item_metakey', 1)) $json['metakey'] = $item -> metakey;
+if($this -> params -> get ('display_item_url', 1)) $json['url'] = $url;
+if($this -> params -> get ('display_item_json', 1)) $json['json'] = $url . (strpos($url, '?') ? '&' : '?') . 'iclayout=' . JRequest::getVar('ilayout') . '&tmpl=' . JRequest::getVar('tmpl');
+if($this -> params -> get ('display_item_params', 0)) $json['params'] = json_decode($item -> params);
+if($this -> params -> get ('display_item_fields', 1)) {
+	$fields = array();
+	if(isset($item -> positions)) {
+		foreach($item -> positions as $position) {
+			foreach($position as $field) {
+				$fields[$field -> name] = array();
+				if($this -> params -> get ('display_item_field_label', 1)) $fields[$field -> name]['label'] = $field -> label;
+				if($this -> params -> get ('display_item_field_value', 1)) {
+					if($item -> fields[$field -> name] -> iscore) {
+						$fields[$field -> name]['value'] = $item -> {$field -> name};
+					}
+					else {
+						$fields[$field -> name]['value'] = $item -> fieldvalues [$field -> id];
+					}
+				}
+				if($this -> params -> get ('display_item_field_display', 1)) $fields[$field -> name]['display'] = $field -> display;
+				
 			}
-			else {
-				$fields[$field -> name] = array(
-					'label' => $field -> label,
-					'value' => $this -> item -> fieldvalues [$field -> id]
-				);
-			}
-			
 		}
-		
 	}
+	$json['fields'] = $fields;
 }
-
-$json = array(
-	'layout' => 'item',
-	'id' => $this -> item -> id,
-	'title' => $this -> item -> title,
-	'alias' => $this -> item -> alias,
-	'author' => $this -> item -> author,
-	'created' => $this -> item -> created,
-	'modified' => $this -> item -> modified,
-	'metadesc' => $this -> metadesc,
-	'metakey' => $this -> metakey,
-	'url' => JRoute::_(JURI::base() . FlexicontentHelperRoute::getItemRoute($this -> item -> slug, $this -> item -> categoryslug)),
-	'json' => JRoute::_(JURI::base() . FlexicontentHelperRoute::getItemRoute($this -> item -> slug, $this -> item -> categoryslug) . '&ilayout=' . JRequest::getVar('clayout') . '&tmpl=' . JRequest::getVar('tmpl')),
-	'fields' => $fields
-);
 
 if (JRequest::getVar('callback', '') != '') {
 	echo JRequest::getVar('callback') . '(' . json_encode($json) . ')';
