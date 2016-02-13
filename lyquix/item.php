a79483d6e25cb81072377849ad3731b317a23663
Ruben Reyes <ruben.reyes@lyquix.com>
2016-02-12 21:05:08 -0500

    Add item CSS section params

--- lyquix/item.php
+++ lyquix/item.php
@@ -3,7 +3,6 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 require_once(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'templates'.DS.'lyquix'.DS.'functions.php');
-
 // Item sections ordering
 $item_sections = $this -> params -> get('item_layout_order', array("group1", "group2", "group3", "group4", "group5", "group6", "group7"));
 
@@ -17,14 +16,14 @@ if (is_array($item_sections)) {
 	$i = 1;
 		
 	echo '<div class="fc-item tmpl-'.str_replace('.items.','',$this->tmpl).' item-'.$this->item->alias.'">' .
-		 '<div class="section-1 ' . $this -> params -> get('css_section_1') . '">';
+		 '<div class="section-1 ' . $this -> params -> get('item_css_section_1') . '">';
 	
 	foreach ($item_sections as $item_section) {
 
 		if (strstr($item_section, 'sep')) {
 
 			$i++;
-			echo '</div><div class="section-' . $i . ' ' . $this -> params -> get('css_section_' . $i) . '">';
+			echo '</div><div class="section-' . $i . ' ' . $this -> params -> get('item_css_section_' . $i) . '">';
 
 		} elseif (strstr($item_section, 'group')) {
 			
@@ -33,11 +32,11 @@ if (is_array($item_sections)) {
 			if(isset($this->item->positions['group_'.$j])){
 				
 				echo '<div class="group-'.$j.' '.$this->params->get('item_css_group_'.$j, '').'">';
-				
+				$counter = 1;
 				foreach ($this->item->positions['group_'.$j] as $field){
-									
+					var_dump($counter);
 					echo lyquixFlexicontentTmpl::renderItemField($this->item,$field);
-					
+					$counter ++;
 				}
 				
 				echo '</div>';
