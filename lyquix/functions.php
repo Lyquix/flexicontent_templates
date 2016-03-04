a6e7d8c973f25ac15e24affa3fa0e4dc1056cfcd
Ruben Reyes <ruben.reyes@lyquix.com>
2016-03-04 16:08:30 -0500

    JSON template updates: - Move code to the lyquix folder (functions.php) to encourage copying of the json folder - Updated all JRequest to the new format in Joomla 3.x - Added links for pagination next/prev , as well as total items, current page, total pages, items per page

--- lyquix/functions.php
+++ lyquix/functions.php
@@ -1186,4 +1186,136 @@ class lyquixFlexicontentTmpl {
 		return $html;
 	}
 
+	function renderJSONcat () {
+		// Set mime type to JSON
+		$doc = &JFactory::getDocument();
+		$doc -> setMimeEncoding('application/json');
+		
+		// get category params
+		$category = $this -> category;
+		$catparams = json_decode($category -> params);
+		
+		// generate urls
+		$tmpl = JFactory::getApplication() -> input -> get('tmpl');
+		$clayout = JFactory::getApplication() -> input -> get('clayout');
+		$limit = JFactory::getApplication() -> input -> get('limit', $this -> pageNav -> limit);
+		$limitstart = JFactory::getApplication() -> input -> get('limitstart', 0);
+		$url = trim(JURI::base(), "/") . JRoute::_(FlexicontentHelperRoute::getCategoryRoute($this -> category -> id));
+		$url_json = $url . '?clayout=' . $clayout . '&tmpl=' . $tmpl . '&limit=' . $limit;
+		$url_prev = $url_json . '&limitstart=' . ($limitstart - $limit);
+		$url_next = $url_json . '&limitstart=' . ($limitstart + $limit);
+		
+		$json = array('layout' => 'category');
+		if($this -> params -> get ('display_cat_id', 1)) $json['id'] = $category -> id;
+		if($this -> params -> get ('display_cat_title', 1)) $json['title'] = $category -> title;
+		if($this -> params -> get ('display_cat_alias', 1)) $json['alias'] = $category -> alias;
+		if($this -> params -> get ('display_cat_description', 1)) $json['description'] = $category -> description;
+		if($this -> params -> get ('display_cat_image', 1)) $json['image'] = $catparams -> image ? JURI::base() . $catparams -> image : '';
+		if($this -> params -> get ('display_cat_created', 1)) $json['created'] = $category -> created_time;
+		if($this -> params -> get ('display_cat_modified', 1)) $json['modified'] = $category -> modified_time;
+		if($this -> params -> get ('display_cat_metadesc', 1)) $json['metadesc'] = $category -> metadesc;
+		if($this -> params -> get ('display_cat_metakey', 1)) $json['metakey'] = $category -> metakey;
+		if($this -> params -> get ('display_cat_url', 1)) $json['url'] = $url;
+		if($this -> params -> get ('display_cat_json', 1)) $json['json'] = $url . (strpos($url, '?') ? '&' : '?') . 'clayout=' . $clayout . '&tmpl=' . $tmpl;
+		if($this -> params -> get ('display_cat_params', 0)) $json['params'] = $catparams;
+
+		$json['total_items'] = $this -> pageNav -> total;
+		$json['items_per_page'] = $this -> pageNav -> limit;
+		$json['current_page'] = $this -> pageNav -> {'pages.current'};
+		$json['total_pages'] = $this -> pageNav -> {'pages.total'};
+		$json['prev_page'] = ($this -> pageNav -> {'pages.current'} <= 1 ? '' : $url_prev);
+		$json['next_page'] = ($this -> pageNav -> {'pages.current'} >= $this -> pageNav -> {'pages.total'} ? '' : $url_next);
+		
+		// process category items
+		$json['items'] = array();
+		
+		foreach($this -> items as $item) {
+			$url = trim(JURI::base(), "/") . JRoute::_(FlexicontentHelperRoute::getItemRoute($item -> slug, $item -> categoryslug));
+			$json_item = array();
+			$json_item['layout'] = 'item';
+			if($this -> params -> get ('display_item_id', 1)) $json_item['id'] = $item -> id;
+			if($this -> params -> get ('display_item_title', 1)) $json_item['title'] = $item -> title;
+			if($this -> params -> get ('display_item_alias', 1)) $json_item['alias'] = $item -> alias;
+			if($this -> params -> get ('display_item_author', 1)) $json_item['author'] = $item -> author;
+			if($this -> params -> get ('display_item_description', 1)) $json_item['description'] = $item -> text;
+			if($this -> params -> get ('display_item_created', 1)) $json_item['created'] = $item -> created;
+			if($this -> params -> get ('display_item_modified', 1)) $json_item['modified'] = $item -> modified;
+			if($this -> params -> get ('display_item_metadesc', 1)) $json_item['metadesc'] = $item -> metadesc;
+			if($this -> params -> get ('display_item_metakey', 1))  $json_item['metakey'] = $item -> metakey;
+			if($this -> params -> get ('display_item_url', 1)) $json_item['url'] = $url;
+			if($this -> params -> get ('display_item_json', 1)) $json_item['json'] = $url . (strpos($url, '?') ? '&' : '?') . 'ilayout=' . $clayout . '&tmpl=' . $tmpl;
+			if($this -> params -> get ('display_item_params', 0)) $json_item['params'] = json_decode($item -> params);
+			if($this -> params -> get ('display_item_fields', 1)) {
+				$fields = array();
+				if(isset($item -> positions)) {
+					foreach($item -> positions as $position) {
+						foreach($position as $field) {
+							$fields[$field -> name] = array();
+							if($this -> params -> get ('display_item_field_label', 1)) $fields[$field -> name]['label'] = $field -> label;
+							if($this -> params -> get ('display_item_field_value', 1)) $fields[$field -> name]['value'] = $item -> fields[$field -> name] -> iscore ? $item -> {$field -> name} : $item -> fieldvalues [$field -> id];
+							if($this -> params -> get ('display_item_field_display', 1)) $fields[$field -> name]['display'] = $field -> display;
+						}
+					}
+				}
+				$json_item['fields'] = $fields;
+			}
+			$json['items'][] = $json_item;
+		}
+		
+		if (JFactory::getApplication() -> input -> get('callback', '') != '') {
+			echo JFactory::getApplication() -> input -> get('callback') . '(' . json_encode($json) . ')';
+		}
+		else {
+			echo json_encode($json);
+		}
+	}
+	
+	function renderJSONitem () {
+		// Set mime type to JSON
+		$doc =& JFactory::getDocument();
+		$doc->setMimeEncoding('application/json');
+		
+		$item = $this -> item;
+		
+		$url = trim(JURI::base(), "/") . JRoute::_(FlexicontentHelperRoute::getItemRoute($item -> slug, $item -> categoryslug));
+		$json = array();
+		$json['layout'] = 'item';
+		if($this -> params -> get ('display_item_id', 1)) $json['id'] = $item -> id;
+		if($this -> params -> get ('display_item_title', 1)) $json['title'] = $item -> title;
+		if($this -> params -> get ('display_item_alias', 1)) $json['alias'] = $item -> alias;
+		if($this -> params -> get ('display_item_author', 1)) $json['author'] = $item -> author;
+		if($this -> params -> get ('display_item_description', 1)) $json['description'] = $item -> text;
+		if($this -> params -> get ('display_item_created', 1)) $json['created'] = $item -> created;
+		if($this -> params -> get ('display_item_modified', 1)) $json['modified'] = $item -> modified;
+		if($this -> params -> get ('display_item_metadesc', 1)) $json['metadesc'] = $item -> metadesc;
+		if($this -> params -> get ('display_item_metakey', 1)) $json['metakey'] = $item -> metakey;
+		if($this -> params -> get ('display_item_url', 1)) $json['url'] = $url;
+		if($this -> params -> get ('display_item_json', 1)) $json['json'] = $url . (strpos($url, '?') ? '&' : '?') . 'iclayout=' . JFactory::getApplication() -> input -> get('ilayout') . '&tmpl=' . JFactory::getApplication() -> input -> get('tmpl');
+		if($this -> params -> get ('display_item_params', 0)) $json['params'] = json_decode($item -> params);
+		if($this -> params -> get ('display_item_fields', 1)) {
+			$fields = array();
+			if(isset($item -> positions)) {
+				foreach($item -> positions as $position) {
+					foreach($position as $field) {
+						$fields[$field -> name] = array();
+						if($this -> params -> get ('display_item_field_label', 1)) $fields[$field -> name]['label'] = $field -> label;
+						if($this -> params -> get ('display_item_field_value', 1)) $fields[$field -> name]['value'] = $item -> fields[$field -> name] -> iscore ? $item -> {$field -> name} : $item -> fieldvalues [$field -> id];
+						if($this -> params -> get ('display_item_field_display', 1)) $fields[$field -> name]['display'] = $field -> display;
+						
+					}
+				}
+			}
+			$json['fields'] = $fields;
+		}
+		
+		if (JFactory::getApplication() -> input -> get('callback', '') != '') {
+			echo JFactory::getApplication() -> input -> get('callback') . '(' . json_encode($json) . ')';
+		}
+		else {
+			echo json_encode($json);
+		}
+		
+
+	}
+
 }
