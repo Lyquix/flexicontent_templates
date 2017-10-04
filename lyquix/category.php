02cfa1c0ebbebe8cc437d8f54468e083ccbe3685
Prasetyo Wibowo <prasetyo@lyquix.com>
2017-10-04 16:24:34 -0400

    Add: tmpl custom object passing to functions.php. Change: method_exists params, to check the custom object and render it from the custom object. Change: Filter rendering, need to add a check to make sure the overriding functions from category-custom.php is top priority.

--- lyquix/category.php
+++ lyquix/category.php
@@ -8,23 +8,25 @@ if (!class_exists('lyquixFlexicontentTmplCustom')) require_once (JPATH_SITE . DS
 // Category sections ordering
 $cat_sections = $this -> params -> get('cat_layout_order', array("buttons", "title", "filters", "alpha", "image", "desc", "map", "subcats", "items", "pagination"));
 
+
 if (!is_array($cat_sections)) {
 	$cat_sections = explode(",", $cat_sections);
 }
 
 if (is_array($cat_sections)) {
 
-	$lyquixFlexicontentTmplCustom = new lyquixFlexicontentTmplCustom($this);
 	$lyquixFlexicontentTmpl = new lyquixFlexicontentTmpl($this);
+	$lyquixFlexicontentTmplCustom = new lyquixFlexicontentTmplCustom($this);
+	$lyquixFlexicontentTmpl -> setTmplCustomObject($lyquixFlexicontentTmplCustom);
 
 	// keeps track of the current section number
 	$i = 0;
 
 	echo '<div class="fc-category tmpl-' . str_replace('.category.', '', $this -> tmpl) . ' cat-' . $this -> category -> alias . ' cat-' . $this -> category -> id 
-		. (method_exists('lyquixFlexicontentTmplCustom','customCatClass') ? ' ' . $lyquixFlexicontentTmplCustom -> customCatClass($this -> category) : '')
+		. (method_exists($lyquixFlexicontentTmplCustom,'customCatClass') ? ' ' . $lyquixFlexicontentTmplCustom -> customCatClass($this -> category) : '')
 		. ($this -> params -> get('css_wrapper') ? ' ' . $this -> params -> get('css_wrapper') : '')
 		. '"'
-		. (method_exists('lyquixFlexicontentTmplCustom','customCatAttrs') ? ' ' . $lyquixFlexicontentTmplCustom -> customCatAttrs($this -> category) : '')
+		. (method_exists($lyquixFlexicontentTmplCustom,'customCatAttrs') ? ' ' . $lyquixFlexicontentTmplCustom -> customCatAttrs($this -> category) : '')
 		.'>';
 
 	foreach ($cat_sections as $cat_section) {
@@ -41,22 +43,28 @@ if (is_array($cat_sections)) {
 		} elseif ($cat_section == 'filters') {
 			$html = '';
 			if ($this-> params -> get('use_filters', 0)) {
-					
-				$html .= '<div class="cat-filters ' . $this-> params -> get('cat_filters_class', '') . '">';
-				$html .= $this-> params -> get('cat_filters_label', '');
-				if($this-> params -> get('cat_filters_engine', 0)) {
-					require_once(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'templates'.DS.'lyquix'.DS.'filters.php');
-					$html .= $lyquixFlexicontentTmpl -> renderFilters();
-					$html .= '</div>';
-					
-				}
-				else {
-					// this is the problem right now, the included file below requires that $this variable refers to the context: flexicontentCategoryView.
-					// since it is called inside the class lyquixFlexicontentTmpl, $this variable refers to the lyquixFlexicontentTmpl...				
+
+				$html .= method_exists($lyquixFlexicontentTmplCustom,'customSectionRendering') ? $lyquixFlexicontentTmplCustom -> customSectionRendering('renderCatFilters') : '';
+				if ($html == '') {
+					$html .= '<div class="cat-filters ' . $this-> params -> get('cat_filters_class', '') . '">';
+					$html .= $this-> params -> get('cat_filters_label', '');
+					if($this-> params -> get('cat_filters_engine', 0)) {
+						require_once(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'templates'.DS.'lyquix'.DS.'filters.php');
+						$html .= $lyquixFlexicontentTmpl -> renderFilters();
+						$html .= '</div>';
+						
+					}
+					else {
+						// this is the problem right now, the included file below requires that $this variable refers to the context: flexicontentCategoryView.
+						// since it is called inside the class lyquixFlexicontentTmpl, $this variable refers to the lyquixFlexicontentTmpl...				
+						echo $html;
+						include (JPATH_SITE . DS . 'components' . DS . 'com_flexicontent' . DS . 'tmpl_common' . DS . 'listings_filter_form_html5.php');
+						echo '</div>';
+						$html = '';
+
+					}
+				} else {
 					echo $html;
-					include (JPATH_SITE . DS . 'components' . DS . 'com_flexicontent' . DS . 'tmpl_common' . DS . 'listings_filter_form_html5.php');
-					echo '</div>';
-					$html = '';
 				}
 			}
 
@@ -65,12 +73,11 @@ if (is_array($cat_sections)) {
 			$html = '';
 			$section = 'renderCat' . ucfirst($cat_section);
 
-			$html .= method_exists('lyquixFlexicontentTmplCustom','customSectionRendering') ? $lyquixFlexicontentTmplCustom -> customSectionRendering($section) : '';
-			
+			$html .= method_exists($lyquixFlexicontentTmplCustom,'customSectionRendering') ? $lyquixFlexicontentTmplCustom -> customSectionRendering($section) : '';
 			if(!$html) {
-				$html .= method_exists('lyquixFlexicontentTmplCustom','customSectionRenderingPretext') ? $lyquixFlexicontentTmplCustom -> customSectionRenderingPretext($section) : '';
+				$html .= method_exists($lyquixFlexicontentTmplCustom,'customSectionRenderingPretext') ? $lyquixFlexicontentTmplCustom -> customSectionRenderingPretext($section) : '';
 				$html .= $lyquixFlexicontentTmpl -> $section();
-				$html .= method_exists('lyquixFlexicontentTmplCustom','customSectionRenderingPosttext') ? $lyquixFlexicontentTmplCustom -> customSectionRenderingPosttext($section) : '';
+				$html .= method_exists($lyquixFlexicontentTmplCustom,'customSectionRenderingPosttext') ? $lyquixFlexicontentTmplCustom -> customSectionRenderingPosttext($section) : '';
 			}
 
 			echo $html;
