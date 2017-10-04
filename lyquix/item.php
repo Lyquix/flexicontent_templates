9a79365694241998defa2af25aa6b81946cf5daf
Prasetyo Wibowo <prasetyo@lyquix.com>
2017-10-04 16:22:39 -0400

    Fix: wrong include files, and add tmplCustomOject passing to functions.php

--- lyquix/item.php
+++ lyquix/item.php
@@ -1,9 +1,8 @@
 <?php
 
 defined('_JEXEC') or die('Restricted access');
-
 require_once (JPATH_SITE . DS . 'components' . DS . 'com_flexicontent' . DS . 'templates' . DS . 'lyquix' . DS . 'functions.php');
-if (!class_exists('lyquixFlexicontentTmplCustom')) require_once (JPATH_SITE . DS . 'components' . DS . 'com_flexicontent' . DS . 'templates' . DS . 'custom' . DS . 'category-custom.dist.php');
+if (!class_exists('lyquixFlexicontentTmplCustom')) require_once (JPATH_SITE . DS . 'components' . DS . 'com_flexicontent' . DS . 'templates' . DS . 'custom' . DS . 'item-custom.dist.php');
 
 // Item sections ordering
 $item_sections = $this -> params -> get('item_layout_order', array("group1", "group2", "group3", "group4", "group5", "group6", "group7"));
@@ -19,11 +18,13 @@ if (is_array($item_sections)) {
 	$lyquixFlexicontentTmpl = new lyquixFlexicontentTmpl($this);
 	$lyquixFlexicontentTmplCustom = new lyquixFlexicontentTmplCustom($this);
 
+	$lyquixFlexicontentTmpl -> setTmplCustomObject($lyquixFlexicontentTmplCustom);
+
 	echo '<div class="fc-item tmpl-' . str_replace('.items.', '', $this -> tmpl) . ' item-' . $this -> item -> alias . ' item-' . $this -> item -> id 
-		. (method_exists('lyquixFlexicontentTmplCustom','customItemClass') ? ' ' . $lyquixFlexicontentTmplCustom -> customItemClass($this -> item) : '')
+		. (method_exists($lyquixFlexicontentTmplCustom,'customItemClass') ? ' ' . $lyquixFlexicontentTmplCustom -> customItemClass($this -> item) : '')
 		. ($this -> params -> get('item_css_wrapper') ? ' ' . $this -> params -> get('css_wrapper') : '')
 		. '"'
-		. (method_exists('lyquixFlexicontentTmplCustom','customItemAttrs') ? ' ' . $lyquixFlexicontentTmplCustom -> customItemAttrs($this -> item) : '')
+		. (method_exists($lyquixFlexicontentTmplCustom,'customItemAttrs') ? ' ' . $lyquixFlexicontentTmplCustom -> customItemAttrs($this -> item) : '')
 		.'>';
 
 	foreach ($item_sections as $item_section) {
