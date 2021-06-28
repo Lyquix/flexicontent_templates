7a541f72413f6cb24dc41c27137200f77c1cc738
Ruben Reyes <ruben.reyes@lyquix.com>
2021-06-28 11:32:24 -0400

    Update category.php

--- redirect/category.php
+++ redirect/category.php
@@ -17,6 +17,7 @@ $redirect_url = $this -> params -> get('redirect_url', '/');
 
 // if HTTP headers have not been sent
 if (!headers_sent()) {
+	header('HTTP/1.1 301 Moved Permanently');
 	header('Location: ' . $redirect_url);
 	exit;
 }
